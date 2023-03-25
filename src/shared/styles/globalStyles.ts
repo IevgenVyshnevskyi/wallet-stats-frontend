19cb253be24fce12149d77651a1763ab7ca181d9
DavidGit999 <itprog999@gmail.com>
2023-03-25 22:28:17 +0200

    refactor(styles): extract fonts to a separate file

--- src/shared/styles/globalStyles.ts
+++ src/shared/styles/globalStyles.ts
@@ -1,52 +1,14 @@
 import { createGlobalStyle } from "styled-components";
-import './variables.ts'
-import InterRegular from '../fonts/Inter/static/Inter-Regular.ttf'
-import InterMedium from '../fonts/Inter/static/Inter-Medium.ttf'
-import InterSemiBold from '../fonts/Inter/static/Inter-SemiBold.ttf'
-import InterBold from '../fonts/Inter/static/Inter-Bold.ttf'
-import InterExtraBold from '../fonts/Inter/static/Inter-ExtraBold.ttf'
 import { ALMOST_BLACK_FOR_TEXT, BASE_1, WHITE } from "./variables";
+import { fontStyles } from "./fontStyles";
 
 export const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: 'Inter';
-    src: url(${InterRegular}) format('truetype');
-    font-weight: 400;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Inter';
-    src: url(${InterMedium}) format('truetype');
-    font-weight: 500;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Inter';
-    src: url(${InterSemiBold}) format('truetype');
-    font-weight: 600;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Inter';
-    src: url(${InterBold}) format('truetype');
-    font-weight: 700;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: 'Inter';
-    src: url(${InterExtraBold}) format('truetype');
-    font-weight: 800;
-    font-style: normal;
-  }
+  ${fontStyles}
 
   * {
     margin : 0;
     padding: 0;
-    font-family: 'Inter';
+    font-family: 'Inter', Arial, sans-serif;
   }
   
   body {
